daad2e08f5ccf36c5a36108de7a1d9cd104c0adf
Coder J <buildingguy@sbcglobal.net>
2022-03-20 16:41:28 -0700

    start seeds

---
+++ seeds/thought-seeds.js
@@ -0,0 +1,49 @@
+const { Thoughts } = require('../models');
+
+const reactionData = [
+    {
+        reactionId: 1,
+        reactionBody: 'Ikr',
+        username: 'jellybeanboy',
+        createdAt: Date()
+    },
+    {
+        reactionId: 2,
+        reactionBody: 'Not to mention, gas prices are skyrocketing',
+        username: 'Steve098',
+        createdAt: Date()
+    }
+];
+
+const thoughtData = [
+    {
+        thoughtText: 'Wow! This new movie is great!',
+        createdAt: Date(),
+        username: 'Steve098',
+        reactions: reactionData[0]
+    },
+    {
+        thoughtText: 'I love the bakery on Elm Street in NYC.',
+        createdAt: Date(),
+        username: 'jellybeanboy',
+        reactions: ''
+    },
+    {
+        thoughtText: 'How is this app better than Instagram?',
+        createdAt: Date(),
+        username: 'Steve098',
+        reactions: ''
+    },
+    {
+        thoughtText: 'It is raining and cold. Netflix and chill for me.',
+        createdAt: Date(),
+        username: 'HolisterFanatic',
+        reactions: ''
+    },
+    {
+        thoughtText: 'Dang! This whole Ukraine and Russia conflict is getting crazy rn',
+        createdAt: Date(),
+        username: 'jellybeanbody',
+        reactions: reactionData[1]
+    }
+];
---
+++ seeds/user-seeds.js
@@ -0,0 +1,22 @@
+const { User } = require('../models');
+
+const userData = [
+    {
+        username: 'jellybeanboy',
+        email: 'jason@yahoo.com',
+        thoughts: [1, 4],
+        friends: [2]
+    },
+    {
+        username: 'HolisterFanatic',
+        email: 'selena@sbcglobal.net',
+        thoughts: [3],
+        friends: []
+    },
+    {
+        username: 'Steve098',
+        email: 'steve@gmail.com',
+        thoughts: [0, 2],
+        friends: [0]
+    }
+];
